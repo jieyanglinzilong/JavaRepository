e6a8c1d9c22a597168d3f07965c1a9144ea1c50d
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2021-01-04 13:24:21 +0800

    计网截图

--- hadoopproject/src/main/java/com/example/mkdir/Main.java
+++ hadoopproject/src/main/java/com/example/mkdir/Main.java
@@ -1,9 +1,9 @@
 package com.example.mkdir;
 
 
+import org.apache.commons.httpclient.URIException;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -20,10 +20,10 @@ public class Main {
         // configuration.set("fs.defaultFS", "hdfs://hadoop102:9000");
         // FileSystem fs = FileSystem.get(configuration);
 
-        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop102:9000"), configuration, "atguigu");
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop101:9000"), configuration, "lin");
 
         // 2 创建目录
-        fs.mkdirs(new Path("/1108/daxian/banzhang"));
+        fs.mkdirs(new Path("/zhou"));
 
         // 3 关闭资源
         fs.close();
@@ -32,11 +32,12 @@ public class Main {
     public void testCopyFromLocalFile() throws IOException, InterruptedException, URISyntaxException {
         // 1 获取文件系统
         Configuration configuration = new Configuration();
+        //设置副本数为2
         configuration.set("dfs.replication", "2");
-        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop1001:9000"), configuration, "lin");
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop101:9000"), configuration, "lin");
 
         // 2 上传文件
-        fs.copyFromLocalFile(new Path("D:/BigData/ncdc/2009.txt"),new Path("/user/2009.txt"));
+        fs.copyFromLocalFile(new Path("D:/BigData/ncdc/2010.txt"),new Path("/user/2010"));
         //fs.copyFromLocalFile(new Path("D:/BigData/ncdc/2009.txt"),  );
 
         // 3 关闭资源
@@ -55,6 +56,71 @@ public class Main {
 
         fs.close();
         System.out.println("over");
+    }
+    @Test
+    public void readRight() throws IOException,InterruptedException, URISyntaxException{
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop101:9000"), configuration, "lin");
+
+        // 2 获取文件详情
+        RemoteIterator<LocatedFileStatus> listFiles = fs.listFiles(new Path("/sun"), true);
+
+        while(listFiles.hasNext()){
+            LocatedFileStatus status = listFiles.next();
+
+            // 输出详情
+            // 文件名称 分组 长度 权限
+            System.out.println(status.getPath().getName()+" "+ status.getGroup()+" "+status.getLen()+" "+status.getPermission());
+            // 长度
+           // System.out.println(status.getLen());
+            // 权限
+            //System.out.println(status.getPermission());
+            // 分组
+            //System.out.println(status.getGroup());
+
+            // 获取存储的块信息
+            BlockLocation[] blockLocations = status.getBlockLocations();
+
+            for (BlockLocation blockLocation : blockLocations) {
+
+                // 获取块存储的主机节点
+                String[] hosts = blockLocation.getHosts();
+
+                for (String host : hosts) {
+                    System.out.print(host+" ");
+                }
+                System.out.println("");
+            }
+
+
+        }
+// 3 关闭资源
+        fs.close();
+
+    }
+    //判断文件夹和文件
+    @Test
+    public void testListStatus() throws IOException, InterruptedException, URISyntaxException{
+
+        // 1 获取文件配置信息
+        Configuration configuration = new Configuration();
+        FileSystem fs = FileSystem.get(new URI("hdfs://hadoop1001:9000"), configuration, "lin");
+
+        // 2 判断是文件还是文件夹
+        FileStatus[] listStatus = fs.listStatus(new Path("/"));
+
+        for (FileStatus fileStatus : listStatus) {
+
+            // 如果是文件
+            if (fileStatus.isFile()) {
+                System.out.println("f:"+fileStatus.getPath().getName());
+            }else {
+                System.out.println("d:"+fileStatus.getPath().getName());
+            }
+        }
+
+        // 3 关闭资源
+        fs.close();
     }
 
 }
